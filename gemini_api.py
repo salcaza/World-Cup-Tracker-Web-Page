9f8617f88fdb437c2a044de5c5c487c61053c484
Kenneth Silvestre <ksilv@bu.edu>
2026-06-24 19:22:04 +0000

    Tested outputs, tuned gemini, and future insights now focus on future games only.

--- gemini_api.py
+++ gemini_api.py
@@ -1,6 +1,6 @@
 import os
 from google import genai 
-from google.genai import types
+from google.genai import types, errors
 import football_api
 import news_api
 
@@ -16,11 +16,15 @@ client = genai.Client(
 
 # Schedule context to feed gemini (re-used print schedule from football_api.py)
 def build_schedule_context(team_name):
-    schedule = get_team_schedule(team_name)
+    schedule = football_api.get_team_schedule(team_name)
 
-    context = "World Cup Schedule for {team_name}:\n"
+    context = f"World Cup Schedule for {team_name}:\n"
 
     for game in schedule:
+        # skip games that are finished
+        finished = str(game.get("finished")).lower()
+        if finished == "true":
+            continue
         date = game.get("local_date", "Date TBD")
         home = game.get("home_team_name_en", "TBD")
         away = game.get("away_team_name_en", "TBD")
@@ -44,13 +48,23 @@ def get_future_insight(team_name):
 
     user_prompt = f"Analyze {team_name}'s 2026 World Cup future matchups. Use {schedule_conext} as context for team schedule." 
 
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        config=types.GenerateContentConfig(
-            system_instruction="You are a professional football analyst for the 2026 World Cup and you are able to provide clear, realistic future insights on teams"
-        ), 
-        contents=user_prompt
-    )
-    print(response.text)
+    try: 
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            config=types.GenerateContentConfig(
+                system_instruction="You are a professional football analyst for the 2026 World Cup and you are able to provide brief, realistic future insights on teams."
+            ), 
+            contents=user_prompt
+        )
+        return print(response.text)
+    
+    # Error handling
+    except errors.ServerError as e:
+        print("Gemini is currently unavailable")
+        print("Error: ", e)
 
+
+if __name__ == "__main__":
+
+    insight = get_future_insight("Mexico")
 
