cfb2ac0a6a491a352e6e643feaa87b43168cc8a2
Sergio Alcazar <sergioalcazar1007@gmail.com>
2026-06-24 18:16:28 +0000

    Updated main.py to allow new options

--- main.py
+++ main.py
@@ -1,24 +1,41 @@
 from football_api import get_team_schedule, print_schedule
-from news_api import get_news
+from news_api import get_news, print_news
 from database import save_headlines, save_schedule, save_team
 
 print("WELCOME TO THE WORLD CUP TRACKER")
 
-team = input("Enter a team you'd like to track: ")
+while true:
+    team = input("Enter a team you'd like to track (or 'quit' to exit): ")
 
-menu = (
-    "MENU OPTIONS:\n"
-    "View Team Schedule (S)\n"
-    "View Recent Headlines (H)\n"
-    "View Future Insights (I)\n"
-)
+    if team.lower() == "quit":
+        break
 
-choice = input(menu)
+    while true:
 
-if choice.upper() == "S":
-    print_schedule(team)
-    print()
-    save = input("Would you like to save this info? (y/n)")
-    if save.lower() == "y":
-        save_schedule(get_team_schedule(team))
-#if choice.upper() == "H":
+        menu = (
+            "\nMENU OPTIONS:\n"
+            "View Team Schedule (S)\n"
+            "View Recent Headlines (H)\n"
+            "View Future Insights (I)\n"
+            "Change Team(C)\n"
+            "Quit(Q)\n"
+        )
+
+        choice = input(menu).upper()
+
+        if choice == "S":
+            print_schedule(team)
+            print()
+            save = input("Would you like to save this info? (y/n)")
+            if save.lower() == "y":
+                save_schedule(get_team_schedule(team))
+        elif choice == "H":
+            print_news(team)
+            print()
+            save = input("Would you like to save this info? (y/n)")
+            if save.lower() == "y":
+                save_headlines(get_news)
+        elif choice == "I":
+            pass
+        else:
+            print("INVALID CHOICE")
